faf0dc3ea8d5e0bb29ec9113a101805815637567
MohammadAlyounes <85723285+MohammadAlyounes@users.noreply.github.com>
2022-03-10 15:43:57 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import roc_auc_score
+import xgboost as xgb
+import numpy as np
+
+
+file = r'D:\CreditScoring.csv'
+df = pd.read_csv(file)
+
+# data cleaning
+df.columns = df.columns.str.lower()
+status_values = {
+    1: 'ok',
+    2: 'default',
+    0: 'unk'
+}
+df.status = df.status.map(status_values)
+
+home_values = {
+    1: 'rent',
+    2: 'owner',
+    3: 'private',
+    4: 'ignore',
+    5: 'parents',
+    6: 'other',
+    0: 'unk'
+}
+
+marital_values = {
+    1: 'single',
+    2: 'married',
+    3: 'widow',
+    4: 'separated',
+    5: 'divorced',
+    0: 'unk'
+}
+df.marital = df.marital.map(marital_values)
+
+records_values = {
+    1: 'no',
+    2: 'yes',
+    0: 'unk'
+}
+df.records = df.records.map(records_values)
+
+job_values = {
+    1: 'fixed',
+    2: 'parttime',
+    3: 'freelance',
+    4: 'others',
+    0: 'unk'
+}
+df.job = df.job.map(job_values)
+
+for c in ['income', 'assets', 'debt']:
+    df[c] = df[c].replace(to_replace=99999999, value=np.nan)
+
+df = df[df.status != 'unk']
+
+# dataset preparation
+
+df_train_full, df_test = train_test_split(df, test_size=0.2, random_state=11)
+y_train = (df_train_full.status == 'default').values
+y_test = (df_test.status == 'default').values
+
+del df_train_full['status']
+del df_test['status']
+
+
+# convert DataFrames into lists of dictionaries and then use one-hot encoding to get the feature matrices
+
+dict_train = df_train_full.fillna(0).to_dict(orient='records')
+dict_test = df_test.fillna(0).to_dict(orient='records')
+
+dv = DictVectorizer(sparse=False)
+X_train = dv.fit_transform(dict_train)
+X_test = dv.transform(dict_test)
+
+# train a XGBoost model
+
+dtrain = xgb.DMatrix(X_train, label=y_train, feature_names=dv.feature_names_)
+dtest = xgb.DMatrix(X_test, label=y_test, feature_names=dv.feature_names_)
+
+
+xgb_params = {
+    'eta': 0.1,
+    'max_depth': 3,
+    'min_child_weight': 1,
+    'objective': 'binary:logistic',
+    'eval_metric': 'auc',
+    'nthread': 8,
+    'seed': 1,
+    'silent': 1
+}
+
+num_trees = 160
+
+model = xgb.train(xgb_params, dtrain, num_boost_round=num_trees)
+
+# evaluate the performance of the model on test set
+
+y_pred_xgb = model.predict(dtest)
+print(roc_auc_score(y_test, y_pred_xgb) * 100, '%')
+
